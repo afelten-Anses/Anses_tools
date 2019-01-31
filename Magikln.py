3c5a1a48e1a9b5fd0de8a6f4eccb9bc545f2396c
Arnaud Felten <arnaud.felten@anses.fr>
2019-01-31 10:42:43 +0100

    Add new script to make symbolic link

---
+++ Magikln.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+import os, sys
+import argparse
+import pymongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+def get_parser():
+    
+    parser = argparse.ArgumentParser(description='make links from artwork outputs for a project id')
+    
+    parser.add_argument('-p', action="store", dest='project',
+						type=str, required=True, help='project name (REQUIRED)')
+    
+    parser.add_argument('-g', action="store", dest='pathogene', 
+                        choices=['Salmonella','Listeria','Bacillus','Clostridium','Staphylococcus'],
+						type=str, required=True, help='pathogene (REQUIRED)')
+    
+    parser.add_argument('--reads', action="store_true", dest='reads', 
+						default=False, help='make links for reads')
+    
+    parser.add_argument('--assembly', action="store_true", dest='assembly', 
+						default=False, help='make links for assemblies')
+    
+    parser.add_argument('--vcf', action="store_true", dest='vcf', 
+						default=False, help='make links for vcf files')
+    
+    parser.add_argument('--genbank', action="store_true", dest='genbank', 
+						default=False, help='make links for genbank files')
+
+    parser.add_argument('--copy', action="store_true", dest='copy', 
+						default=False, help='copy files instead of making links')
+
+    return parser
+
+
+#main function	
+def main():
+    
+    # Get arguments 
+    parser=get_parser()
+	
+    # Print parser.help if no arguments
+    if len(sys.argv)==1:
+        
+		parser.print_help()
+		sys.exit(1)
+	
+    Arguments=parser.parse_args()
+	
+    if Arguments.copy :
+        commande = "cp "
+    else :
+        commande = "ln -sF "
+        
+    if Arguments.reads :
+        os.mkdir("ARTwork_reads")
+    if Arguments.assembly :
+        os.mkdir("ARTwork_assembly")
+    if Arguments.vcf :
+        os.mkdir("ARTwork_vcf")	
+    if Arguments.genbank :
+        os.mkdir("ARTwork_genbank")	
+
+    uri = "mongodb://Kindle:Amazon@SAS-PP-LSCALC1/GAMeRdb"
+    client = MongoClient(uri)
+    db = client.GAMeRdb
+    genomes = db.GENOME
+
+    if genomes.find({"Project":Arguments.project}).count() == 0:
+        print("ERROR : project " + Arguments.project + " doesn't exist in GAMeRdb !")
+        sys.exit(1)
+
+    listDoc = genomes.find({"Project":Arguments.project, "Phylogeny.Genus":Arguments.pathogene})
+        
+    for document in listDoc :
+        
+        if Arguments.reads :
+            
+            pwd_pair1 = document["Reads"]["FASTQ_pair1"].replace("DATA","data")
+            filename_pair1 = pwd_pair1.split('/')[-1]
+            pwd_pair2 = document["Reads"]["FASTQ_pair2"].replace("DATA","data")
+            filename_pair2 = pwd_pair2.split('/')[-1]
+            
+            if Arguments.copy :
+                os.system("cp /global/bio/" + pwd_pair1 + " ARTwork_reads/.")
+                os.system("cp /global/bio/" + pwd_pair2 + " ARTwork_reads/.")
+                
+            else :
+                os.system("ln -sF /global/bio/" + pwd_pair1 + " $PWD/ARTwork_reads/" + filename_pair1)
+                os.system("ln -sF /global/bio/" + pwd_pair2 + " $PWD/ARTwork_reads/" + filename_pair2)
+         
+        if Arguments.vcf :
+            
+            pwd = document["Reads"]["VCF"].replace("DATA","data")
+            filename = pwd.split('/')[-1]
+            
+            if Arguments.copy :
+               os.system("cp /global/bio/" + pwd + " ARTwork_vcf/.") 
+            else :
+               os.system("ln -sF /global/bio/" + pwd + " $PWD/ARTwork_vcf/" + filename) 
+                
+        if Arguments.assembly :
+            
+            pwd = document["Genome"]["Assembly"].replace("DATA","data")
+            filename = pwd.split('/')[-1]
+            
+            if Arguments.copy :
+               os.system("cp /global/bio/" + pwd + " ARTwork_assembly/.") 
+            else :
+               os.system("ln -sF /global/bio/" + pwd + " $PWD/ARTwork_assembly/" + filename) 
+                
+        if Arguments.genbank :
+            
+            pwd = document["Genome"]["GBK"].replace("DATA","data")
+            filename = pwd.split('/')[-1]
+            
+            if Arguments.copy :
+               os.system("cp /global/bio/" + pwd + " ARTwork_genbank/.") 
+            else :
+               os.system("ln -sF /global/bio/" + pwd + " $PWD/ARTwork_genbank/" + filename) 
+                
+	
+if __name__ == "__main__":
+	main()	
